1ce7d5569a7e6708382a4080726e6a54836cee35
yifuyu88-lab <yifuyu88@gmail.com>
2026-02-15 17:29:33 +0900

    Delete src directory

--- src/src/db.js
+++
@@ -1,8 +0,0 @@
-const Database = require("better-sqlite3");
-const path = require("path");
-
-const db = new Database(path.join(__dirname, "..", "data.sqlite"));
-db.pragma("journal_mode = WAL");
-db.pragma("foreign_keys = ON");
-
-module.exports = db;
